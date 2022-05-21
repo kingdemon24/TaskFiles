67360394564dea99e85c628ba4e5d999ae3c87f2
Jenish Budhathoki <102845646+MoMoSir206@users.noreply.github.com>
2022-05-21 15:22:40 +0545

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,70 +1,71 @@
-# #1. Write a python program to reverse a given list using while loop.
-# # a=[1,2,3,4]
+#1. Write a python program to reverse a given list using while loop.
 # a=[1,2,3,4]
-# output=[ ]
-# i=len(a)-1
-# while i>=0:
-#     output=a.reverse()
-#     i=i-1
-# print(a)
+a=[1,2,3,4]
+output=[ ]
+i=len(a)-1
+while i>=0:
+    output=a.reverse()
+    i=i-1
+print(a)
 
 
 
-#2.Write a python program to reverse a string using while loop.
-# a="python"
+# 2.Write a python program to reverse a string using while loop.
+a="python"
 
-# a="python"
-# output=" "
-# i=len(a)-1
-# while i>=0:
-#     output=output+a[i]
-#     i=i-1
-# print(output)
+a="python"
+output=" "
+i=len(a)-1
+while i>=0:
+    output=output+a[i]
+    i=i-1
+print(output)
 
 
 # 3.Write a python program to print a multiplication table of any number using while loop.
-# n=int(input("Enter a number for multiplication:"))
-# i=1
-# while i<=10:
-#     print(n,"*",i,"=",n*i)
-#     i=i+1
+n=int(input("Enter a number for multiplication:"))
+i=1
+while i<=10:
+    print(n,"*",i,"=",n*i)
+    i=i+1
 
 # 5.Write a program to print the following using while loop
-# a. first 10 even numbers
-# b.first 10 odd numbers
-# c.first 10 natural numbers
-# i=0
-# while i<=8:
-#     i=i+2
-#     print(i)
-# i=1
-# while i<=20:
-#     i=i+2
-#     print(i)
-# i=0
-# while i<=9:
-#     i=i+1
-#     print(i)
-#6.Write for loop statement to print the following series:
-# 10 20 30 --------300
-# a=10
-# while a<300:
-#
-#     a=a+10
-#     print(a,end=" ")
+a. first 10 even numbers
+b.first 10 odd numbers
+c.first 10 natural numbers
+i=0
+while i<=8:
+    i=i+2
+    print(i)
+i=1
+while i<=20:
+    i=i+2
+    print(i)
+i=0
+while i<=9:
+    i=i+1
+    print(i)
+    
+# 6.Write for loop statement to print the following series:
+10 20 30 --------300
+a=10
+while a<300:
 
-#7. Write a while loop statement to print the following series:
-# 105 98 -------7
-# a=112
-# while a>=7:
-#     a=a-7
-#     print(a,end=" ")
+    a=a+10
+    print(a,end=" ")
 
-#8. Write a program to print the factorial of a number accepted from user.
+# 7. Write a while loop statement to print the following series:
+105 98 -------7
+a=112
+while a>=7:
+    a=a-7
+    print(a,end=" ")
 
-# n=int(input("Enter a number to calculate factorial:"))
-# fact=1
-# while n>0:
-#     fact=fact*n
-#     n=n-1
-# print(fact)
+# 8. Write a program to print the factorial of a number accepted from user.
+
+n=int(input("Enter a number to calculate factorial:"))
+fact=1
+while n>0:
+    fact=fact*n
+    n=n-1
+print(fact)
