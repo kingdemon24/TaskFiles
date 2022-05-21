8a61420b47c3e85b194fa2aa32dfa10cbaaf0570
Jenish Budhathoki <102845646+MoMoSir206@users.noreply.github.com>
2022-05-21 15:21:46 +0545

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,140 +1,140 @@
-# #Qno.1
-# a = int(input("enter a number :"))
-# b = int(input("enter a number :"))
-# if a == b:
-#     print("1")
-# elif a > b:
-#     print("2")
-# else:
-#     print("3")
+#Qno.1
+a = int(input("enter a number :"))
+b = int(input("enter a number :"))
+if a == b:
+    print("1")
+elif a > b:
+    print("2")
+else:
+    print("3")
 
 
-# #Qno.2
-# a = int(input("enter a number :"))
-# b = int(input("enter a number :"))
-# c = int(input("enter a number :"))
-# d = int(input("enter a number :"))
-# if a == b and c == d:
-#     print("hello")
-# else:
-#     print("")
+#Qno.2
+a = int(input("enter a number :"))
+b = int(input("enter a number :"))
+c = int(input("enter a number :"))
+d = int(input("enter a number :"))
+if a == b and c == d:
+    print("hello")
+else:
+    print("")
 
-# #Qno.4
-# a = int(input("enter a number :"))
-# b = int(input("enter a number :"))
-# c = int(input("enter a number :"))
-# d = int(input("enter a number :"))
-# if a == b or c == d:
-#     print("hello")
-# else:
-#     print("")
-
-
-# #Qno.5
-# x = int(input("enter a number :"))
-# if x > 0:
-#     print("True")
-# elif x < 0:
-#     print("False")
-# else:
-#     print("zero")
+#Qno.4
+a = int(input("enter a number :"))
+b = int(input("enter a number :"))
+c = int(input("enter a number :"))
+d = int(input("enter a number :"))
+if a == b or c == d:
+    print("hello")
+else:
+    print("")
 
 
-# #Qno.5
-# a = int(input("enter a number: "))
-# if a%2 == 0:
-#     print("the number is even")
-# if a%2 != 0:
-#     print("the number is odd")
-# else:
-#     print("Enter a valid number")
+#Qno.5
+x = int(input("enter a number :"))
+if x > 0:
+    print("True")
+elif x < 0:
+    print("False")
+else:
+    print("zero")
 
 
-##Qno.6
-# math = int(input("Enter your marks in maths: "))
-# science = int(input("Enter your marks in science: "))
-# english = int(input("Enter your marks in english: "))
-# computer = int(input("Enter your marks in computer: "))
-# total_marks = math + science + english + computer
-# percentage = total_marks /40
-# if percentage >= 70:
-#     print(f"Your total marks is {total_marks}")
-#     print(f"Congratulations! Youv'e scored distinction with {percentage}%.")
-# elif percentage >=60:
-#     print(f"Your total marks is {total_marks}")
-#     print(f"Well done! Youv'e scored first division with {percentage}%.")
-# elif percentage >=40:
-#     print(f"Your total marks is {total_marks}")
-#     print(f"Youv'e passed with {percentage}%.")
-# else:
-#     print(f"Your total marks is {total_marks}")
-#     print(f"Better luck next time you've failed with {percentage}%.")
+#Qno.5
+a = int(input("enter a number: "))
+if a%2 == 0:
+    print("the number is even")
+if a%2 != 0:
+    print("the number is odd")
+else:
+    print("Enter a valid number")
 
 
-# #Qno.7
-# a = "APPLE"
-# print(a.lower())
+#Qno.6
+math = int(input("Enter your marks in maths: "))
+science = int(input("Enter your marks in science: "))
+english = int(input("Enter your marks in english: "))
+computer = int(input("Enter your marks in computer: "))
+total_marks = math + science + english + computer
+percentage = total_marks /40
+if percentage >= 70:
+    print(f"Your total marks is {total_marks}")
+    print(f"Congratulations! Youv'e scored distinction with {percentage}%.")
+elif percentage >=60:
+    print(f"Your total marks is {total_marks}")
+    print(f"Well done! Youv'e scored first division with {percentage}%.")
+elif percentage >=40:
+    print(f"Your total marks is {total_marks}")
+    print(f"Youv'e passed with {percentage}%.")
+else:
+    print(f"Your total marks is {total_marks}")
+    print(f"Better luck next time you've failed with {percentage}%.")
 
 
-# # Qno.8
-# name = input("enter your name :")
-# age = int(input("Enter your age :"))
-# adress = input("Enter your adress :")
-# print(f"Hello {name}.Have a good day.")
-# print(f"Your age is {age}.")
-# print(f"You're currently living in {adress}")
+#Qno.7
+a = "APPLE"
+print(a.lower())
+
+
+# Qno.8
+name = input("enter your name :")
+age = int(input("Enter your age :"))
+adress = input("Enter your adress :")
+print(f"Hello {name}.Have a good day.")
+print(f"Your age is {age}.")
+print(f"You're currently living in {adress}")
 
 
 
-# #Qno.9
-# radius = int(input("enter the radius of circle :"))
-# area = 2*(3.14)*(radius)**2
-# print(f"The area of circle is :{area}.")
+#Qno.9
+radius = int(input("enter the radius of circle :"))
+area = 2*(3.14)*(radius)**2
+print(f"The area of circle is :{area}.")
 
 
-#Qno.10
+Qno.10
 a = int(input("Enter the number of students in class a:"))
 b = int(input("Enter the number of students in class b:"))
 c = int(input("Enter the number of students in class c:"))
 desk = (a + b + c)/2
-z = round(desk)  #round is used so that we get a extra desk if the number of studenta is odd.
+z = round(desk)  
 print(f"The total number of desk required is :{z}")
 
 
-# #Qno.11
-# a = int(input("Enter first number :"))
-# b = int(input("Enter second number :"))
-# c = int(input("Enter third number :"))
-# if a <b and a < c:
-#     print(f"The samllest number is :{a}")
-# elif b < c and b < a:
-#     print(f"The samllest number is :{b}")
-# else:
-#     print(f"The samllest number is :{c}")
+#Qno.11
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+c = int(input("Enter third number :"))
+if a <b and a < c:
+    print(f"The samllest number is :{a}")
+elif b < c and b < a:
+    print(f"The samllest number is :{b}")
+else:
+    print(f"The samllest number is :{c}")
 
 
 
-# #Qno.12
-# a = int(input("Enter first number :"))
-# b = int(input("Enter second number :"))
-# c = int(input("Enter third number :"))
-# sum = a + b + c
-# print(f"The sum of the numbers is:{sum}")
+#Qno.12
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+c = int(input("Enter third number :"))
+sum = a + b + c
+print(f"The sum of the numbers is:{sum}")
 
 
-# #Qno.13
-# l = int(input("enter the length of triangle:"))
-# b = int(input("Enter the base of triangle:"))
-# area = (l * b)/2
-# print(f"The are of right-angled triangle is :{area}")
+#Qno.13
+l = int(input("enter the length of triangle:"))
+b = int(input("Enter the base of triangle:"))
+area = (l * b)/2
+print(f"The are of right-angled triangle is :{area}")
 
 
-# #Qno.14
-# n = int(input("Enter the number of students :"))
-# k = int(input("enter the number of apples :"))
-# get = k //  n
-# left = k % n
-# print(f"{n} students will get {get} apple each.")
-# print(f"The remaning number of apples are {left}")
+#Qno.14
+n = int(input("Enter the number of students :"))
+k = int(input("enter the number of apples :"))
+get = k //  n
+left = k % n
+print(f"{n} students will get {get} apple each.")
+print(f"The remaning number of apples are {left}")
 
 
